fd8c202dff04c7669be4bf28c28dc56a5cbc14aa
Leonardo Breda <leonardo-breda1@hotmail.com>
2024-12-03 17:21:38 -0300

    Decorator login_required para bloquear paginas apenas para usuarios autenticados

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,3 +119,5 @@ MEDIA_URL = 'imagens/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'fazer_login'
--- loja/views.py
+++ loja/views.py
@@ -3,6 +3,7 @@ from .models import *
 import uuid
 from .utils import filtrar_produtos, preco_minimo_maximo, ordenar_produtos
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 
@@ -226,6 +227,7 @@ def adicionar_endereco(request):
         return render(request, 'adicionar_endereco.html', context=context)
 
 
+@login_required
 def minha_conta(request):
     return render(request, 'usuario/minha_conta.html')
 
@@ -303,6 +305,7 @@ def criar_conta(request):
     return render(request, 'usuario/criar_conta.html', context=context)
 
 
+@login_required
 def fazer_logout(request):
     logout(request)
     return redirect('fazer_login')
